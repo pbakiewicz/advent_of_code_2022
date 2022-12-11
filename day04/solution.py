2269e6311425457c9fe41f205a5094c855a0aa93
Pawel Bakiewicz <pawel@bakiewicz.io>
2022-12-11 12:14:34 +0100

    Day 4. Using sets can improve readines.
    By using set it is clearer what we want to check here. It is more declarative way of programming.

--- day04/solution.py
+++ day04/solution.py
@@ -8,17 +8,14 @@ def part_1() -> int:
         first, second = line.split(",")
         first_start, first_end = first.split("-")
         second_start, second_end = second.split("-")
-
-        if int(first_start) == int(second_start):
+        first_section = set(range(int(first_start), int(first_end) + 1))
+        second_section = set(range(int(second_start), int(second_end) + 1))
+        if first_section == second_section:
             solution += 1
-        elif int(first_start) > int(second_start):
-            # first can only be included by second
-            if int(first_end) <= int(second_end):
-                solution +=1
-        else:
-            # second can only be included by first
-            if int(first_end) >= int(second_end):
-                solution+=1
+        elif first_section.issubset(second_section):
+            solution += 1
+        elif second_section.issubset(first_section):
+            solution += 1
 
     return solution
 
